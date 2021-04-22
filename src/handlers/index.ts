7acf62b43afc7872cbffaea60fde22948f191fb1
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-04-22 08:31:09 +0200

    expose get-metadata-service handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,11 @@
 import  createTimestampHandler  from "./timestamp";
 import  createWhoamiHandler  from "./whoami";
+import getMetadataService from "./get-metadata-service";
 import { TimestampService } from "../services/timestamp";
 import { WhoamiService } from "../services/whoami";
 
 export default {
   timestamp: createTimestampHandler(new TimestampService()),
-  whoami: createWhoamiHandler(new WhoamiService())
+  whoami: createWhoamiHandler(new WhoamiService()),
+  metadataService: getMetadataService
 }
